c28c5d7e50b56805324b9011dfa326028121bacf
Tony <ordaz.manrique@gmail.com>
2020-05-16 18:22:53 -0500

    removing irrelevant notes

--- checkmypass.py
+++ checkmypass.py
@@ -4,24 +4,16 @@ import sys
 import fileinput
 
 
-# password API url
-# ALWAYS HASH A PASSWORD
-# K-Anonymity - just give the first 5 char of the hashed password
-
-
 def request_api_data(query_char):
     url = 'https://api.pwnedpasswords.com/range/' + query_char
 # respond
     res = requests.get(url)
     if res.status_code != 200:
-        # Error if we make a mistake
         raise RuntimeError(
             f'Error fetching: {res.status_code}, check the API and try again')
     return res
 
 
-# all the hashes that match our password
-# receives the response and the actual hash to check
 # check the hash_to_check and loop through all the responses(hashes)
 def get_passwords_leaks_count(hashes, hash_to_check):
     # tuple that has the hash and the count
@@ -34,14 +26,11 @@ def get_passwords_leaks_count(hashes, hash_to_check):
 
 
 # converts password to SHA1
-def pwned_api_check(password):  # password123
-    # check if pass exists in API response
+def pwned_api_check(password):
     shaw1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    # first 5 char of the hashed pass and the tail that is the rest
     first5_char, tail = shaw1password[:5], shaw1password[5:]
     # send to API
     response = request_api_data(first5_char)
-    #  print(response)
     return get_passwords_leaks_count(response, tail)
 
 
